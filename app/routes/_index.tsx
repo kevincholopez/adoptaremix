a5b5dd11dcef04fb533f0d33f56b5284eac74c30
kevincholopez <ingkevinlopez@outlook.com>
2024-04-11 18:29:20 -0500

    ajuste de carousel, toca ajustarlo bien para responsive

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -20,7 +20,9 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-
+  const listImages = [{
+    "imagen": "https://hips.hearstapps.com/hmg-prod/images/gettyimages-1422023439-64f1eaf518ace.jpg?crop=1xw:0.84375xh;center,top&resize=1200:*"
+  }]
   return (
     <main>
       <div className='bg-repeat-x backgroundBanner'>
@@ -60,9 +62,7 @@ export default function Index() {
       <div className='secondSpace'>
         <h1 className={`titleLobby col-start-2 col-span-3 centerTitles p-10`}>Posibles mascotas en adopción para dueños de mascotas</h1>
       </div>
-      <div className="h-96">
-        <CarouselComponent />
-      </div>
+      <CarouselComponent listImages={listImages}/>
     </main>
   );
 }
--- app/shared/components/carousel/carousel.js
+++ app/shared/components/carousel/carousel.js
@@ -1,15 +1,17 @@
-import { Carousel } from 'flowbite-react'
+import { Carousel } from "flowbite-react";
 
-export default function CarouselComponent(listImages) {
+function CarouselComponent({ listImages }) {
+    const listAnimals = listImages.map((item, id) =>
+        <img src={item.imagen} alt="image" width={600} height={400}></img>
+    );
+
     return (
-        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+        <div className="h-screen w-1/2">
             <Carousel>
-                {listImages.listImages.length !== 0 ? listImages.listImages.map((item, id) => {
-                    return (
-                        <img width={400} height={400} src={item.image} alt="2" />
-                    )
-                }) : <img width={400} height={400} alt="2" />}
+                {listAnimals}
             </Carousel>
         </div>
-    )
+    );
 }
+
+export default CarouselComponent;
